bc82825acb68543effaf09952abaaa6b49253086
Goutham Vangala <Goutham.Vangala@maine.gov>
2016-06-29 16:13:35 -0400

    test for weeks

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('test for week', done);
+    request(app).get('/').expect('test for weeks', done);
   });
 });
